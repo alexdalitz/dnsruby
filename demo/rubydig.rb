8a479189f280c22076bbc11923a01bcec400a86c
alexd <alexd@14de010c-f11f-0410-ba76-d4c1185d02e1>
2009-04-23 14:25:40 +0000

    Removing DLV checks from rubydig - will make digdlv and digitar for demo purposes

--- demo/rubydig.rb
+++ demo/rubydig.rb
@@ -23,9 +23,6 @@ include Dnsruby
 res = Dnsruby::Resolver.new
 zt=Dnsruby::ZoneTransfer.new
   
-    dlv_key = RR.create("dlv.isc.org. IN DNSKEY 257 3 5 BEAAAAPHMu/5onzrEE7z1egmhg/WPO0+juoZrW3euWEn4MxDCE1+lLy2 brhQv5rN32RKtMzX6Mj70jdzeND4XknW58dnJNPCxn8+jAGl2FZLK8t+ 1uq4W+nnA3qO2+DL+k6BD4mewMLbIYFwe0PG73Te9fZ2kJb56dhgMde5 ymX4BI/oQ+cAK50/xvJv00Frf8kw6ucMTwFlgPe+jnGxPPEmHAte/URk Y62ZfkLoBAADLHQ9IrS2tryAe7mbBZVcOwIeU/Rw/mRx/vwwMCTgNboM QKtUdvNXDrYJDSHZws3xiRXF1Rf+al9UmZfSav/4NWLKjHzpT59k/VSt TDN0YUuWrBNh")
-    Dnssec.add_dlv_key(dlv_key)
-
 if (ARGV && (ARGV[0] =~ /^@/))
   nameserver = ARGV.shift
   if (nameserver == "@auth")
