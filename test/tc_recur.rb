fe3514e742263f10d1a963692f71a2ad5271061d
alexd <alexd@14de010c-f11f-0410-ba76-d4c1185d02e1>
2009-05-15 10:28:18 +0000

    Removing redundant reminders

--- test/tc_recur.rb
+++ test/tc_recur.rb
@@ -12,14 +12,4 @@ class TestRecur < Test::Unit::TestCase
 #    ret = r.query_dorecursion("aaa.bigzone.uk-dnssec.nic.uk", Dnsruby::Types.DNSKEY)
 #    ret = r.query_dorecursion("uk-dnssec.nic.uk", Dnsruby::Types.DNSKEY)
   end
-
-  def test_use_as_a_resolver
-    # @TODO@ Try to use the recursor as a normal resolver.
-    print "Test recursor as normal resolver!\n"
-  end
-
-  def test_recursor_caching
-    # @TODO@ Test the recursor caching.
-    print "Test recursor caching!\n"
-  end
 end
