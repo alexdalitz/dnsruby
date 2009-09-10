bb12fb2705c7568ad7782e53c2b67c234bf80df9
alexd <alexd@14de010c-f11f-0410-ba76-d4c1185d02e1>
2009-09-10 14:00:47 +0000

    Adding copyright notices and removing event_machine_interface.rb

--- lib/Dnsruby/Cache.rb
+++ lib/Dnsruby/Cache.rb
@@ -1,4 +1,21 @@
-# This class implements a cache.
+#--
+#Copyright 2007 Nominet UK
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+#++
+
+
+## This class implements a cache.
 # It stores data under qname-qclass-qtype tuples.
 # Each tuple indexes a CacheData object (which
 # stores a Message, and an expiration).
--- lib/Dnsruby/validator_thread.rb
+++ lib/Dnsruby/validator_thread.rb
@@ -1,3 +1,19 @@
+#--
+#Copyright 2007 Nominet UK
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+#++
+
 module Dnsruby
   # Takes care of the validation for the SelectThread. If queries need to be
   # made in order to validate the response, then a separate thread is fired up
