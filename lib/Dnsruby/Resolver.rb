a92a03515343613b764ed6791f33acd2ef5daa3b
alexd <alexd@14de010c-f11f-0410-ba76-d4c1185d02e1>
2009-12-03 10:28:05 +0000

    Adding message to timeout when no nameservers configured

--- lib/Dnsruby/Resolver.rb
+++ lib/Dnsruby/Resolver.rb
@@ -331,7 +331,7 @@ module Dnsruby
       if (@single_resolvers.length == 0)
         Thread.start {
           sleep(@query_timeout == 0 ? 1 : @query_timeout)
-          args[1].push([client_query_id, nil, ResolvTimeout.new])
+          args[1].push([client_query_id, nil, ResolvTimeout.new("Query timed out - no nameservers configured")])
         }
       end
       return client_query_id
