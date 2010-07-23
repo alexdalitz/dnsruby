b227712efdccf12247f85a15e9ca4226b82f770d
alexd <alexd@14de010c-f11f-0410-ba76-d4c1185d02e1>
2010-07-23 08:06:17 +0000

    Upping version number to 1.48 for imminent release

--- lib/dnsruby.rb
+++ lib/dnsruby.rb
@@ -104,7 +104,7 @@ require 'Dnsruby/TheLog'
 module Dnsruby
 
   # @TODO@ Remember to update version in dnsruby.gemspec!
-  VERSION = 1.47
+  VERSION = 1.48
   def Dnsruby.version
     return VERSION
   end
