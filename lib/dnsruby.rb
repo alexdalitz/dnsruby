e47c66716615d7b3de306fc8608548238b82b309
alexd <alexd@14de010c-f11f-0410-ba76-d4c1185d02e1>
2011-11-17 11:05:08 +0000

    Update version to 1.53 for imminent release

--- lib/dnsruby.rb
+++ lib/dnsruby.rb
@@ -104,7 +104,7 @@ require 'Dnsruby/TheLog'
 module Dnsruby
 
   # @TODO@ Remember to update version in dnsruby.gemspec!
-  VERSION = 1.52
+  VERSION = 1.53
   def Dnsruby.version
     return VERSION
   end
