7597a4f3d89dd2c5e02d78d43eb67ff642889716
Keith Bennett <kbennett@verisign.com>
2014-11-11 19:02:21 -0500

    Add ;nil note.

--- test/tc_gpos.rb
+++ test/tc_gpos.rb
@@ -3,9 +3,10 @@ require_relative 'spec_helper'
 require 'dnsruby'
 include Dnsruby
 
+# ";nil" added temporarily for ease of use when pasting into irb/pry.
 
 GPOS_QUERY_BYTES = "\xE7\u0001\u0001 \u0000\u0001\u0000\u0000\u0000\u0000\u0000\u0001\u0001g\adnsruby\u0003com" +
-    "\u0000\u0000\e\u0000\u0001\u0000\u0000)\u0010\u0000\u0000\u0000\u0000\u0000\u0000\u0000"
+    "\u0000\u0000\e\u0000\u0001\u0000\u0000)\u0010\u0000\u0000\u0000\u0000\u0000\u0000\u0000"; nil
 
 GPOS_QUERY = Message.decode(GPOS_QUERY_BYTES); nil
 
@@ -14,7 +15,7 @@ GPOS_QUERY = Message.decode(GPOS_QUERY_BYTES); nil
 RESPONSE_BINARY = "\xE7\x01\x85\x90\x00\x01\x00\x01\x00\x01\x00\x01\x01g\adnsruby\x03com" +
     "\x00\x00\e\x00\x01\xC0\f\x00\e\x00\x01\x00\t:\x80\x00\x0F\x0420.0\x0430.0\x0410.0" +
     "\xC0\x0E\x00\x02\x00\x01\x00\t:\x80\x00\x05\x02ns\xC0\x0E\xC0F\x00\x01\x00\x01\x00" +
-    "\t:\x80\x00\x04\xC0\xA8\x01\n"
+    "\t:\x80\x00\x04\xC0\xA8\x01\n"; nil
 
 RESPONSE = Message.decode(RESPONSE_BINARY); nil
 
