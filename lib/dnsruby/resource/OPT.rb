801ac55152d85cd6523abb592ea2ad6b6c38c55e
Craig E. Despeaux <cedespeaux@verisign.com>
2014-11-18 10:31:09 -0500

    Preserve previous format twiddlings

--- lib/dnsruby/resource/OPT.rb
+++ lib/dnsruby/resource/OPT.rb
@@ -167,11 +167,66 @@ module Dnsruby
         raise NotImplementedError
       end
 
+      def get_ip_addr(opt, family)
+        pad_format_string = family == 1 ? 'x3C' : 'x15C'
+        ip_addr = [0].pack(pad_format_string)
+
+        num_to_copy = opt.data.size - 5
+        if num_to_copy > 0
+          ip_addr[0..num_to_copy] = opt.data[4..(num_to_copy + 4)]
+        end
+        ip_addr
+      end
+
+      def get_client_subnet(opt)
+        family = opt.data[1].unpack('C')[0]
+        return "Unsupported(family=#{family})" unless[1,2].include?(family)
+
+        source_netmask = opt.data[2].unpack('C')[0]
+        scope_netmask = opt.data[3].unpack('C')[0]
+
+        case family
+        when 1
+          return "#{IPAddr::ntop(get_ip_addr(opt,family))}/#{source_netmask}/#{scope_netmask}"
+        when 2
+          new_ipv6 = IPAddr.new(IPAddr::ntop(get_ip_addr(opt,family)), Socket::AF_INET6)
+          return "#{new_ipv6}/#{source_netmask}/#{scope_netmask}"
+        end
+      end
+
+      def set_client_subnet(subnet)
+        family = 1
+        scope_netmask = 0
+        ip, source_netmask = subnet.split('/')
+        source_netmask = source_netmask.to_i
+        if subnet == "0.0.0.0/0"
+          edns_client_subnet = RR::OPT::Option.new(8, [family, source_netmask, scope_netmask].pack("xcc*"))
+        else
+          ip_address = IPAddr.new(ip)
+          family = 2 if ip_address.ipv6?
+          num_addr_bytes = source_netmask / 8
+          num_addr_bytes = num_addr_bytes + 1 if source_netmask % 8 > 0
+          edns_client_subnet = RR::OPT::Option.new(8, [family, source_netmask, scope_netmask].pack("xcc*") +
+            ip_address.hton.slice(0, num_addr_bytes))
+        end
+        self.options = [edns_client_subnet]
+      end
+
+      def edns_client_subnet
+        return nil if @options.nil?
+        subnet_option = @options.detect { |option| option.code == 8 }
+        subnet_option ? get_client_subnet(subnet_option) : nil
+      end
+
       def to_s
-        ret = "OPT pseudo-record : payloadsize #{payloadsize}, xrcode #{xrcode.code}, version #{version}, flags #{flags}"
+        ret = "OPT pseudo-record : payloadsize #{payloadsize}, xrcode #{xrcode.code}, version #{version}, flags #{flags}\n"
         if @options
           @options.each do |opt|
-            ret = ret + " " + opt.to_s
+            if opt.code == 8
+              ret = ret + "CLIENT-SUBNET: #{get_client_subnet(opt)}"
+            else
+              ret = ret + " " + opt.to_s
+            end
           end
         end
         ret = ret + "\n"
@@ -198,7 +253,7 @@ module Dnsruby
             options.push(Option.new(code, data))
           end
         end
-        return self.new([options])
+        return self.new(0, 0, options)
       end
 
       class Option
