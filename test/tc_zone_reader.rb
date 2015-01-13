18eecadc2b128b4c13de05b2e159f0a6f0831f2f
Thomas Geirhovd <thomas@linted.net>
2015-01-13 22:04:39 +0100

    Test multiple ways of accessing the zone reader

--- test/tc_zone_reader.rb
+++ test/tc_zone_reader.rb
@@ -5,9 +5,7 @@ include Dnsruby
 
 class ZoneReaderTest < Minitest::Test
 
-  def test_txt_zonefile
-    reader = Dnsruby::ZoneReader.new("example.com.")
-    zone = reader.process_file("#{File.dirname(__FILE__)}/zone_file.txt")
+  def check_zone_data_is_valid(zone)
     assert(zone[0].serial == 1993112101)
     assert(zone[1].rdata == "ns1.example.com.")
     assert(zone[2].rdata == "ns2.example.com.")
@@ -17,6 +15,39 @@ class ZoneReaderTest < Minitest::Test
     assert(zone[6].rdata == "2001:DB8::10")
     #assert(zone[7].rdata == "www.example.com.")
     assert(zone[8].rdata == "www.example.com.")
+  end
+
+  def test_process_file_with_filename
+    reader = Dnsruby::ZoneReader.new("example.com.")
+    zone = reader.process_file("#{File.dirname(__FILE__)}/zone_file.txt")
+    check_zone_data_is_valid(zone)
+  end
+
+  def test_process_file_with_file_object
+    reader = Dnsruby::ZoneReader.new("example.com.")
+    file = File.new("#{File.dirname(__FILE__)}/zone_file.txt")
+    zone = reader.process_file(file)
+    check_zone_data_is_valid(zone)
+    assert(file.closed? == true)
+  end
+
+  def test_process_io_with_file_object
+    reader = Dnsruby::ZoneReader.new("example.com.")
+    file = File.new("#{File.dirname(__FILE__)}/zone_file.txt")
+    zone = reader.process_io(file)
+    check_zone_data_is_valid(zone)
+    assert(file.closed? == false)
+    file.close
+  end
+
+  def test_process_io_with_stringio_object
+    reader = Dnsruby::ZoneReader.new("example.com.")
+    string = File.read("#{File.dirname(__FILE__)}/zone_file.txt")
+    stringio = StringIO.new(string)
+    zone = reader.process_io(stringio)
+    check_zone_data_is_valid(zone)
+    assert(stringio.closed? == false)
+    stringio.close
   end
 end
 
