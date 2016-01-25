31a2a6b4b533f056c6e18ec9439ba0f65bc6b638
Kyle Drake <kyle@kyledrake.net>
2016-01-24 22:12:19 -0800

    ruby 2.3.0 deprecates TimeoutError, use Timeout::Error

--- lib/dnsruby.rb
+++ lib/dnsruby.rb
@@ -161,7 +161,7 @@ module Dnsruby
   end
 
   # A timeout error raised while querying for a resource
-  class ResolvTimeout < TimeoutError
+  class ResolvTimeout < Timeout::Error
   end
 
   # The requested domain does not exist
