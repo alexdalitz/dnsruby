360e4a75f04c9c489f0b917f69bbbd86c9b7d842
Alex Dalitz <alex@caerkettontech.com>
2016-05-09 20:52:24 +0100

    fix indentation

--- lib/dnsruby/message/encoder.rb
+++ lib/dnsruby/message/encoder.rb
@@ -59,16 +59,16 @@ class MessageEncoder #:nodoc: all
   end
 
   def put_labels(d, do_canonical)
-      d.each_index do |i|
-        domain = d[i..-1].join('.')
-        if !do_canonical && (idx = @names[domain])
-          self.put_pack('n', 0xc000 | idx)
-          return
-        else
-          @names[domain] = @data.length
-          self.put_label(d[i])
-        end
+    d.each_index do |i|
+      domain = d[i..-1].join('.')
+      if !do_canonical && (idx = @names[domain])
+        self.put_pack('n', 0xc000 | idx)
+        return
+      else
+        @names[domain] = @data.length
+        self.put_label(d[i])
       end
+    end
     @data << "\0"
   end
 
